3c04ef991043bafa1e230a73d734da4713f7004d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:22:10 +0300

    wip

--- src/ProjectCreation.java
+++ src/ProjectCreation.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class ProjectCreation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int projects = Integer.parseInt(sc.nextLine());
+
+        System.out.printf("The architect %s" +
+                " will need %d hours to complete %d project/s.", name, projects * 3, projects);
     }
 }
