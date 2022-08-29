0f4bbe1802acb9cdb067590e2f175f68a6a237a7
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:32:51 +0300

    wip

--- src/USDtoBGN.java
+++ src/USDtoBGN.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class USDtoBGN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        double lv = Double.parseDouble(sc.nextLine()) * 1.79549;
+        System.out.printf("%.2f", lv);
     }
 }
