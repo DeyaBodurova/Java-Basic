251237a21796cf8ed7662fd2c0e88bd560b24df1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:29:57 +0300

    wip

--- src/YardGreening.java
+++ src/YardGreening.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class YardGreening {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        double oneSqrM = 7.61;
+        double totallMeters = Double.parseDouble(sc.nextLine());
+        double sum = totallMeters * oneSqrM;
+        double discount = sum * 0.18;
+        double finalSum = sum - discount;
+        System.out.printf("The final price is: %.2f lv.",finalSum);
+        System.out.printf("The discount is: %.2f lv.",discount);
     }
 }
