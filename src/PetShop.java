dabb339811a3cc25eef7969f8a369e61eed2148c
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:25:28 +0300

    wip

--- src/PetShop.java
+++ src/PetShop.java
@@ -5,5 +5,7 @@ public class PetShop {
         Scanner sc = new Scanner(System.in);
         int dogsPackage = Integer.parseInt(sc.nextLine());
         int catsPackage = Integer.parseInt(sc.nextLine());
+        double sum = dogsPackage * 2.5 + (catsPackage * 4);
+        System.out.printf("%.2f lv.", sum);
     }
 }
