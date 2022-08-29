3c3ce504b342ed6f349f6801f8ebbded4aa4464d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:19:02 +0300

    wip

---
+++ src/ConcatenateData.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConcatenateData {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.nextLine();
+        String lastName = sc.nextLine();
+        String age = sc.nextLine();
+        String town = sc.nextLine();
+        System.out.println("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town + ".");
+    }
+}
