7305d33e43323faf14aa0537ecf32f3cf52bd4e4
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:11:10 +0300

    wip

---
+++ src/RectangleArea.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.nextLine());
+        int b = Integer.parseInt(sc.nextLine());
+
+        int area = a * b;
+        System.out.println(area);
+    }
+}
