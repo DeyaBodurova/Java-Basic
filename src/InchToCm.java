53f65e985d3d9ca19e9bf86ad39a3e51e06d6cde
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:14:05 +0300

    wip

---
+++ src/InchToCm.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InchToCm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum = Double.parseDouble(sc.nextLine()) * 2.54;
+        System.out.println(sum);
+    }
+}
