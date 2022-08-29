a4e0503bdae57ed44c6553b729ee425e35439e8e
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-29 21:23:34 +0300

    wip

--- src/PetShop.java
+++ src/PetShop.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PetShop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int dogsPackage = Integer.parseInt(sc.nextLine());
+        int catsPackage = Integer.parseInt(sc.nextLine());
     }
 }
