699f2cea238fd0981935614751cb6117dcd2135e
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-01 22:14:01 +0300

    wip

--- src/RadiansToDegrees.java
+++ src/RadiansToDegrees.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class RadiansToDegrees {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double angleRadians = Double.parseDouble(sc.nextLine());
+        double result = angleRadians * 180 /Math.PI;
+        System.out.println(result);
     }
 }
