da7e27346adbc6ce515176d49fd38be0d6630872
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-01 22:53:51 +0300

    wip

--- src/SuppliesForSchool.java
+++ src/SuppliesForSchool.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class SuppliesForSchool {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int pens = Integer.parseInt(sc.nextLine());
+        int markers = Integer.parseInt(sc.nextLine());
+        int  cleaningSupplies= Integer.parseInt(sc.nextLine());
+        int discount = Integer.parseInt(sc.nextLine());
     }
 }
