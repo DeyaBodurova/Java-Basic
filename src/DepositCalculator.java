6649ae24b34e54ee77b08c1aadc259be7859e05d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-01 22:17:04 +0300

    wip

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class DepositCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double deposit = Double.parseDouble(sc.nextLine());
+        int period = Integer.parseInt(sc.nextLine());
+        double percent = Double.parseDouble(sc.nextLine());
+
     }
 }
