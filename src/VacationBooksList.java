837f2d0b636ea102660761a3256a9be179496ef8
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-01 22:22:15 +0300

    wip

--- src/VacationBooksList.java
+++ src/VacationBooksList.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class VacationBooksList {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int pages = Integer.parseInt(sc.nextLine());
+        int pagesPerHour = Integer.parseInt(sc.nextLine());
+        int days = Integer.parseInt(sc.nextLine());
+
 
     }
 }
