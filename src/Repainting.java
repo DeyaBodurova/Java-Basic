4b914aff08d1093b979692d76210b1b8bd20664f
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-03 23:30:12 +0300

    wip

--- src/Repainting.java
+++ src/Repainting.java
@@ -4,5 +4,7 @@ public class Repainting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int naylon = Integer.parseInt(sc.nextLine());
+        int paint = Integer.parseInt(sc.nextLine());
+        int paint = Integer.parseInt(sc.nextLine());
     }
 }
