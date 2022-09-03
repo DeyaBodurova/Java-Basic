4d202ccc03456f9ab952482a3971aafa8375bf9e
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-03 23:34:17 +0300

    wip

--- src/Repainting.java
+++ src/Repainting.java
@@ -8,6 +8,7 @@ public class Repainting {
         int thinner = Integer.parseInt(sc.nextLine());
         int hours = Integer.parseInt(sc.nextLine());
 
-        double sum = ((naylon + 2) * 1.5) + (paint * 14.5)
+        double sum = ((naylon + 2) * 1.5) + ((paint + 0.01) * 14.5)
+                +
     }
 }
