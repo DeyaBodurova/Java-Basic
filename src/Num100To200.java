08b95af93ea46d0f3fcd5dbc4e31334b1c3e0d14
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-04 21:43:57 +0300

    wip

---
+++ src/Num100To200.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Num100To200 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        if(n<100){
+            System.out.println("Less than 100");
+        } else if(n<=200){
+            System.out.println("Between 100 and 200");
+        } else {
+            System.out.println("Greater than 200");
+        }
+    }
+}
