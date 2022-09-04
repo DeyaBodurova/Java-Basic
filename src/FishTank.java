a9c2768cd216b5d1ad422c6fef3f404186d2728d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-04 21:24:46 +0300

    wip

--- src/FishTank.java
+++ src/FishTank.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class FishTank {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = Integer.parseInt(sc.nextLine());
+        int wight = Integer.parseInt(sc.nextLine());
+        int height = Integer.parseInt(sc.nextLine());
+        double percent = Double.parseDouble(sc.nextLine());
 
+        double volume = length * wight * height;
+        double volumeLitters =volume*0.001;
+        double neededLitters = volumeLitters *(percent*0.01);
+        System.out.println(neededLitters);
     }
 }
