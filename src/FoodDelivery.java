3a9e8fb73956a8423fcab6ccd1e0745d78344f87
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-04 21:04:02 +0300

    wip

--- src/FoodDelivery.java
+++ src/FoodDelivery.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class FoodDelivery {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int chickenMenus = Integer.parseInt(sc.nextLine());
+        int fishMenus = Integer.parseInt(sc.nextLine());
+        int veganMenus = Integer.parseInt(sc.nextLine());
     }
 }
