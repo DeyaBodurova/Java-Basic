2de2894ab94a0fd2295ee0addcfaa78d5bd18b76
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-04 21:30:23 +0300

    wip

--- src/ExcellentResult.java
+++ src/ExcellentResult.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class ExcellentResult {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int grade = Integer.parseInt(sc.nextLine());
+        if(grade>=5){
+            System.out.println("Excellent!");
+        }
     }
 }
