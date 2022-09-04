c752c9b38cd83745162fa9c6fc4a889836fc6b97
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-04 21:17:28 +0300

    wip

--- src/BasketballEquipment.java
+++ src/BasketballEquipment.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class BasketballEquipment {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tax = Integer.parseInt(sc.nextLine());
+        double sneakers = tax - (tax * 0.4);
+        double clothes = sneakers - (sneakers*0.2);
+        double ball = clothes /4.0;
+        double accessories = ball /5.0;
 
+        double sum = tax + sneakers + clothes
+                + ball + accessories;
+        System.out.println(sum);
     }
 }
