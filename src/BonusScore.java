52df56e2167ea448e7942b288e267c7d575454ea
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-06 20:09:02 +0300

    wip

--- src/BonusScore.java
+++ src/BonusScore.java
@@ -4,6 +4,18 @@ public class BonusScore {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = Integer.parseInt(sc.nextLine());
+        int bonus = 0;
 
+        if (num < 100) {
+            bonus += 5;
+        } else if (num > 100 && num <= 1000) {
+            bonus += num * 0.2;
+        } else if (num > 1000) {
+            bonus += num * 0.1;
+        }
+
+        if (num % 2 == 0) {
+            bonus += 1;
+        }
     }
 }
