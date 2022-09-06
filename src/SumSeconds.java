4223d152b84527b5d62e5f3e41603d09c95d8a0c
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-06 19:55:19 +0300

    wip

--- src/SumSeconds.java
+++ src/SumSeconds.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class SumSeconds {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int firstTime = Integer.parseInt(sc.nextLine());
+        int secondTime = Integer.parseInt(sc.nextLine());
+        int thirdTime = Integer.parseInt(sc.nextLine());
+
     }
 }
