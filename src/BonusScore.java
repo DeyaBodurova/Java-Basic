ec81e421542710b6b810da9f959d6d7e6c35183e
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-06 20:06:09 +0300

    wip

--- src/BonusScore.java
+++ src/BonusScore.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class BonusScore {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
 
     }
 }
