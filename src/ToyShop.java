7d553474a2388a33092d05654439f38c698abef2
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-08 21:25:33 +0300

    wip

--- src/ToyShop.java
+++ src/ToyShop.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class ToyShop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double tripPrice = Double.parseDouble(sc.nextLine());
+        int puzzleCount = Integer.parseInt(sc.nextLine());
+        int dollCount = Integer.parseInt(sc.nextLine());
+        int teddyBearCount = Integer.parseInt(sc.nextLine());
+        int minionsCount = Integer.parseInt(sc.nextLine());
+        int truckCount = Integer.parseInt(sc.nextLine());
 
     }
 }
