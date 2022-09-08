46925cf479bfa54db74bcb0c5c76377710c91ae1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-08 21:15:33 +0300

    wip

--- src/TimePlus15Minutes.java
+++ src/TimePlus15Minutes.java
@@ -7,10 +7,10 @@ public class TimePlus15Minutes {
         int minutes = Integer.parseInt(sc.nextLine());
 
         int sum = (hour*60) + minutes+15;
-        if(sum>23){
+        int newHour= sum/60;
+        if(newHour>23){
             sum=0;
         }
-        int newHour= sum/60;
         int newMin= sum%60;
         if(newMin<10){
             System.out.printf(newHour+":0"+newMin);
