f01d9102c677d462a3c695811c3d8c5adf8eacf2
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-08 21:42:32 +0300

    wip

--- src/ToyShop.java
+++ src/ToyShop.java
@@ -12,19 +12,19 @@ public class ToyShop {
 
         int totalCountToys = puzzleCount + dollCount + teddyBearCount
                 + minionsCount + truckCount;
-        double totalToysSum = (puzzleCount*2.6) +
-                (dollCount*3.0) + (teddyBearCount*4.1)
-                + (minionsCount * 8.2) + (truckCount*2.0);
+        double totalToysSum = (puzzleCount * 2.6) +
+                (dollCount * 3.0) + (teddyBearCount * 4.1)
+                + (minionsCount * 8.2) + (truckCount * 2.0);
 
-        if(totalCountToys >= 50) {
+        if (totalCountToys >= 50) {
             totalToysSum = totalToysSum - (totalToysSum * 0.25);
         }
-        totalToysSum = totalToysSum - (totalToysSum *0.1);
+        totalToysSum = totalToysSum - (totalToysSum * 0.1);
 
-        if(tripPrice >= totalToysSum){
-            System.out.printf("Yes! %.2f lv left.",tripPrice - totalToysSum);
+        if (tripPrice >= totalToysSum) {
+            System.out.printf("Yes! %.2f lv left.", tripPrice - totalToysSum);
         } else {
-            System.out.printf("Not enough money! %.2f lv needed.",Math.abs(totalToysSum-tripPrice));
+            System.out.printf("Not enough money! %.2f lv needed.", Math.abs(totalToysSum - tripPrice));
         }
 
     }
