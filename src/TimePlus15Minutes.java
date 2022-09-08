322df7d7da76118f261dcbf0bfbaefc2c1c75872
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-08 20:56:32 +0300

    wip

--- src/TimePlus15Minutes.java
+++ src/TimePlus15Minutes.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class TimePlus15Minutes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int hour = Integer.parseInt(sc.nextLine());
+        int minutes = Integer.parseInt(sc.nextLine());
+
+
     }
 }
