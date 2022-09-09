71cb6d974742e9b835cd381edaf1ae844e9b35c1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-09 22:04:56 +0300

    wip

--- src/WorldSwimmingRecord.java
+++ src/WorldSwimmingRecord.java
@@ -7,9 +7,8 @@ public class WorldSwimmingRecord {
         double distanceMeter = Double.parseDouble(sc.nextLine());
         double swimmingTimeMeterPerSec = Double.parseDouble(sc.nextLine());
 
-        for (int i = 0; i < distanceMeter; i++) {
-            if (i % 15 == 0) {
-            }
-        }
+        double swimmingTimeInSec = distanceMeter * swimmingTimeMeterPerSec;
+        double slowing = distanceMeter / 15;
+        double totalTime = swimmingTimeInSec + slowing;
     }
 }
