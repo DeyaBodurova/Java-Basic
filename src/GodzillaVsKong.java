d469f780dbae57d7b938a6a0a14581122bb95ad3
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-09 21:45:46 +0300

    wip

--- src/GodzillaVsKong.java
+++ src/GodzillaVsKong.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class GodzillaVsKong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double budget = Double.parseDouble(sc.nextLine());
+        int statist = Integer.parseInt(sc.nextLine());
+        double clothes = Double.parseDouble(sc.nextLine());
+        double decor = budget * 0.1;
+
+        double sum = (statist * clothes) + decor;
+
+        if (budget < sum) {
+            System.out.println("Not enough money!");
+            System.out.printf("Wingard needs %.2f leva more.", Math.abs(sum - budget));
+        }
     }
 }
