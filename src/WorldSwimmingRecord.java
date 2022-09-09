1844beb54a455fb6269fc7dc62faf90b705586e3
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-09 22:00:07 +0300

    wip

--- src/WorldSwimmingRecord.java
+++ src/WorldSwimmingRecord.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class WorldSwimmingRecord {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double recordSec = Double.parseDouble(sc.nextLine());
 
     }
 }
