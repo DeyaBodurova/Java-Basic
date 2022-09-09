ac6237e0a76b45d2b218dcd772b32c15b489fb3e
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-09 22:01:11 +0300

    wip

--- src/WorldSwimmingRecord.java
+++ src/WorldSwimmingRecord.java
@@ -4,6 +4,8 @@ public class WorldSwimmingRecord {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double recordSec = Double.parseDouble(sc.nextLine());
+        double distanceMeter = Double.parseDouble(sc.nextLine());
+        double swimmingTimeMeterPerSec = Double.parseDouble(sc.nextLine());
 
     }
 }
