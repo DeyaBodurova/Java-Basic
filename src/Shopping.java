a437d8e65b7dbc0de3d00a31f7e1288a166f73bb
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-11 19:34:16 +0300

    wip

--- src/Shopping.java
+++ src/Shopping.java
@@ -4,5 +4,8 @@ public class Shopping {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double budget = Double.parseDouble(sc.nextLine());
+        int videoCardsCount= Integer.parseInt(sc.nextLine());
+        int CPUCount= Integer.parseInt(sc.nextLine());
+        int RAMCount= Integer.parseInt(sc.nextLine());
     }
 }
