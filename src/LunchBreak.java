5ba3b86ad131ff65888a9434822fe563127dc8c1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-11 19:44:01 +0300

    wip

--- src/LunchBreak.java
+++ src/LunchBreak.java
@@ -4,6 +4,7 @@ public class LunchBreak {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String movie = sc.nextLine();
+        int time = Integer.parseInt(sc.nextLine());
 
     }
 }
