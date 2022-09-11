d28749af44c63481c7ab786e06fd34dd39415203
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-11 19:37:01 +0300

    wip

--- src/Shopping.java
+++ src/Shopping.java
@@ -8,6 +8,10 @@ public class Shopping {
         int CPUCount= Integer.parseInt(sc.nextLine());
         int RAMCount= Integer.parseInt(sc.nextLine());
 
+        double videoPrice = videoCardsCount * 250.0;
+        double cpuPrice = CPUCount * (videoPrice*0.35);
+        double ramPrice = RAMCount * (videoPrice*0.1);
 
+        if(){}
     }
 }
