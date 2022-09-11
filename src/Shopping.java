71e0443ec8ab9d869fc6884ce2b73c5d2af6afcd
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-11 19:33:06 +0300

    wip

--- src/Shopping.java
+++ src/Shopping.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Shopping {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double budget = Double.parseDouble(sc.nextLine());
     }
 }
