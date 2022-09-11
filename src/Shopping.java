ddb4cc7537cd25c0a98602dddbd530cad8d1b17d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-11 19:38:33 +0300

    wip

--- src/Shopping.java
+++ src/Shopping.java
@@ -4,14 +4,17 @@ public class Shopping {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double budget = Double.parseDouble(sc.nextLine());
-        int videoCardsCount= Integer.parseInt(sc.nextLine());
-        int CPUCount= Integer.parseInt(sc.nextLine());
-        int RAMCount= Integer.parseInt(sc.nextLine());
+        int videoCardsCount = Integer.parseInt(sc.nextLine());
+        int CPUCount = Integer.parseInt(sc.nextLine());
+        int RAMCount = Integer.parseInt(sc.nextLine());
 
         double videoPrice = videoCardsCount * 250.0;
-        double cpuPrice = CPUCount * (videoPrice*0.35);
-        double ramPrice = RAMCount * (videoPrice*0.1);
+        double cpuPrice = CPUCount * (videoPrice * 0.35);
+        double ramPrice = RAMCount * (videoPrice * 0.1);
+        double total = videoPrice + cpuPrice + ramPrice;
 
-        if(){}
+        if (videoCardsCount > CPUCount) {
+            total = total - (total * 0.15);
+        }
     }
 }
