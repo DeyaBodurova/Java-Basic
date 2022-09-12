8cf390b8d4086663d62b30873b97bb60792ca650
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-12 22:11:03 +0300

    wip

--- src/AreaOfFigures.java
+++ src/AreaOfFigures.java
@@ -6,24 +6,24 @@ public class AreaOfFigures {
         String figure = sc.nextLine();
         double area =0;
         switch (figure) {
-            case "square":
+            case "square" -> {
                 double length = Double.parseDouble(sc.nextLine());
                 area = length * length;
-                break;
-            case "rectangle":
+            }
+            case "rectangle" -> {
                 double a = Double.parseDouble(sc.nextLine());
                 double b = Double.parseDouble(sc.nextLine());
                 area = a * b;
-                break;
-            case "circle":
+            }
+            case "circle" -> {
                 double radius = Double.parseDouble(sc.nextLine());
                 area = Math.PI * (radius * radius);
-                break;
-            case "triangle":
+            }
+            case "triangle" -> {
                 double c = Double.parseDouble(sc.nextLine());
                 double d = Double.parseDouble(sc.nextLine());
                 area = (c * d) / 2;
-                break;
+            }
         }
         System.out.printf("%.3f",area);
     }
