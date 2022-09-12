45c703c156ef4fd9072c9e97f4d8948a269ee3b2
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-12 22:10:29 +0300

    wip

--- src/LunchBreak.java
+++ src/LunchBreak.java
@@ -10,16 +10,16 @@ public class LunchBreak {
         double lunchTime = timeBreak / 8.0;
         double relaxTime = timeBreak / 4.0;
 
-        double totalTime = timeBreak-(lunchTime + relaxTime);
+        double totalTime = timeBreak - (lunchTime + relaxTime);
 
-        if(timeEpisode < totalTime){
+        if (timeEpisode < totalTime) {
             System.out.printf("You have enough time to watch %s and left with %.0f minutes free time.", movie,
-                    Math.ceil(totalTime-timeEpisode));
-        } else if(timeEpisode == totalTime) {
+                    Math.ceil(totalTime - timeEpisode));
+        } else if (timeEpisode == totalTime) {
             System.out.printf("You have enough time to watch %s and left with 0 minutes free time.", movie);
         } else {
-            System.out.printf("You don't have enough time to watch %s, you need %.0f more minutes.",movie,
-                    Math.ceil(timeEpisode-totalTime));
+            System.out.printf("You don't have enough time to watch %s, you need %.0f more minutes.", movie,
+                    Math.ceil(timeEpisode - totalTime));
         }
     }
 }
