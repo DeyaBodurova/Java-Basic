334498f3648f08a34193cfd4d8eccdef537276bd
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-13 22:06:49 +0300

    wip

--- src/WorldSwimmingRecord.java
+++ src/WorldSwimmingRecord.java
@@ -8,13 +8,13 @@ public class WorldSwimmingRecord {
         double swimmingTimeMeterPerSec = Double.parseDouble(sc.nextLine());
 
         double swimmingTimeInSec = distanceMeter * swimmingTimeMeterPerSec;
-       int temp = (int) (distanceMeter/15);
-        double slowing = temp *12.5;
+        int temp = (int) (distanceMeter / 15);
+        double slowing = temp * 12.5;
         double totalTime = swimmingTimeInSec + slowing;
 
-        if(totalTime < recordSec){
+        if (totalTime < recordSec) {
             System.out.printf("Yes, he succeeded!" +
-                    " The new world record is %.2f seconds.",totalTime);
+                    " The new world record is %.2f seconds.", totalTime);
         } else {
             System.out.printf("No, he failed! " +
                     "He was %.2f seconds slower.", totalTime - recordSec);
