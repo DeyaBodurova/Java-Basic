a03964dc203d0ed5789d4502cf63793ca4f085da
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-13 22:17:31 +0300

    wip

--- src/PasswordGuess.java
+++ src/PasswordGuess.java
@@ -4,7 +4,7 @@ public class PasswordGuess {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String pass = sc.nextLine();
-        if(pass.equals("s3cr3t!P@ssw0rd")){
+        if (pass.equals("s3cr3t!P@ssw0rd")) {
             System.out.println("Welcome");
         } else {
             System.out.println("Wrong password!");
