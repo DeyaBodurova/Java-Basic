f4e5e71a647a68f428eb215b17450a8a6b1e4a80
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-13 22:05:24 +0300

    wip

--- src/GreetingByName.java
+++ src/GreetingByName.java
@@ -4,6 +4,6 @@ public class GreetingByName {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
-        System.out.printf("Hello, %s!",name);
+        System.out.printf("Hello, %s!", name);
     }
 }
