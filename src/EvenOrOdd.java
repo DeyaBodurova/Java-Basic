3cc01dc58aef6025dd00a61c4afd1e1b49e71c17
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-14 21:52:02 +0300

    wip

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -4,7 +4,7 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        if(n%2==0){
+        if (n % 2 == 0) {
             System.out.println("even");
         } else {
             System.out.println("odd");
