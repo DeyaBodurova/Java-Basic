6a6030ea594d1eddb7d6001d4d1187c18015b570
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-14 21:09:28 +0300

    wip

--- src/LunchBreak.java
+++ src/LunchBreak.java
@@ -21,6 +21,5 @@ public class LunchBreak {
             System.out.printf("You don't have enough time to watch %s, you need %.0f more minutes.", movie,
                     Math.ceil(timeEpisode - totalTime));
         }
-
     }
 }
