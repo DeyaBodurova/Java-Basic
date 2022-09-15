8922baf96394b3f0cb6f9cf4308a1cbf7d11a43a
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-15 22:27:43 +0300

    wip

--- src/ToyShop.java
+++ src/ToyShop.java
@@ -12,8 +12,7 @@ public class ToyShop {
 
         int totalCountToys = puzzleCount + dollCount + teddyBearCount + minionsCount + truckCount;
         double totalToysSum = (puzzleCount * 2.6) +
-                (dollCount * 3.0) + (teddyBearCount * 4.1)
-                + (minionsCount * 8.2) + (truckCount * 2.0);
+                (dollCount * 3.0) + (teddyBearCount * 4.1) + (minionsCount * 8.2) + (truckCount * 2.0);
 
         if (totalCountToys >= 50) {
             totalToysSum = totalToysSum - (totalToysSum * 0.25);
