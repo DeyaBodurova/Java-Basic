ac886caf3028053610671054d4483d57f47f3245
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-15 22:27:32 +0300

    wip

--- src/ToyShop.java
+++ src/ToyShop.java
@@ -10,8 +10,7 @@ public class ToyShop {
         int minionsCount = Integer.parseInt(sc.nextLine());
         int truckCount = Integer.parseInt(sc.nextLine());
 
-        int totalCountToys = puzzleCount + dollCount + teddyBearCount
-                + minionsCount + truckCount;
+        int totalCountToys = puzzleCount + dollCount + teddyBearCount + minionsCount + truckCount;
         double totalToysSum = (puzzleCount * 2.6) +
                 (dollCount * 3.0) + (teddyBearCount * 4.1)
                 + (minionsCount * 8.2) + (truckCount * 2.0);
