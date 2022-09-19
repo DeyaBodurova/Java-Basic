392acf6a43e380a5d97e81793d84171a90486f72
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-19 17:38:57 +0300

    wip

--- src/FoodDelivery.java
+++ src/FoodDelivery.java
@@ -6,8 +6,7 @@ public class FoodDelivery {
         int chickenMenus = Integer.parseInt(sc.nextLine());
         int fishMenus = Integer.parseInt(sc.nextLine());
         int veganMenus = Integer.parseInt(sc.nextLine());
-        double sum = (chickenMenus * 10.35) + (fishMenus * 12.4)
-                + (veganMenus * 8.15);
+        double sum = (chickenMenus * 10.35) + (fishMenus * 12.4) + (veganMenus * 8.15);
         double dessert = sum * 0.2;
         double total = sum + dessert + 2.5;
         System.out.println(total);
