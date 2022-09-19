44f16731c0c3151c4bff2d9039f983a523d04261
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-19 17:36:02 +0300

    wip

--- src/BonusScore.java
+++ src/BonusScore.java
@@ -13,7 +13,6 @@ public class BonusScore {
         } else if (num > 1000) {
             bonus += num * 0.1;
         }
-
         if (num % 2 == 0) {
             bonus += 1;
         } else if (num % 5 == 0) {
