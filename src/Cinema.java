1d3b77a487385357d96db5ab9726b5fe0eebde32
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-20 17:47:22 +0300

    wip

--- src/Cinema.java
+++ src/Cinema.java
@@ -4,6 +4,7 @@ public class Cinema {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String projection = sc.nextLine();
+        int rows = Integer.parseInt(sc.nextLine());
 
     }
 }
