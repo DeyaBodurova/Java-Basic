0c987d16eb3f54879cbf2294a11d0d791cf121fb
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-20 21:20:05 +0300

    wip

--- src/SummerOutfit.java
+++ src/SummerOutfit.java
@@ -5,6 +5,49 @@ public class SummerOutfit {
         Scanner sc = new Scanner(System.in);
         int degrees = Integer.parseInt(sc.nextLine());
         String dayTime = sc.nextLine();
-
+        String outfit = "";
+        String shoes = "";
+        if (10 <= degrees && degrees <= 18) {
+            switch (dayTime) {
+                case "Morning":
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                    break;
+                case "Afternoon":
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+            }
+        } else if (18 <= degrees && degrees <= 24) {
+            switch (dayTime) {
+                case "Morning":
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+                case "Afternoon":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+            }
+        } else if (degrees >= 25) {
+            switch (dayTime) {
+                case "Morning":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+                case "Afternoon":
+                    outfit= "Swim Suit";
+                    shoes = "Barefoot";
+                    break;
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+            }
+        }
+        System.out.printf("It's %d degrees, get your %s" +
+                " and %s.", degrees, outfit, shoes);
     }
 }
