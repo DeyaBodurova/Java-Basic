f8dab94ce50c0ba999b78c2906e27903b611ce04
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-20 18:02:39 +0300

    wip

--- src/SummerOutfit.java
+++ src/SummerOutfit.java
@@ -4,6 +4,6 @@ public class SummerOutfit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int degrees = Integer.parseInt(sc.nextLine());
-
+        String dayTime = sc.nextLine();
     }
 }
