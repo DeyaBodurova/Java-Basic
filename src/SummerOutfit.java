133f7dbf916da2d18d43fa291b834cbc64b2371f
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-20 18:00:03 +0300

    wip

--- src/SummerOutfit.java
+++ src/SummerOutfit.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class SummerOutfit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int degrees = Integer.parseInt(sc.nextLine());
 
     }
 }
