a1348d9d877a3941baa1984641fe7ef280b48822
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-21 18:22:30 +0300

    wip

--- src/NewHouse.java
+++ src/NewHouse.java
@@ -6,37 +6,46 @@ public class NewHouse {
         String flower = sc.nextLine();
         int count = Integer.parseInt(sc.nextLine());
         int budget = Integer.parseInt(sc.nextLine());
-        double change;
         double total;
-        double price = 0;
-        switch (flower){
-            case "Roses":
-                price = 5.0;
-                break;
-            case "Dahlias":
-                price = 3.8;
-                break;
-            case "Tulips":
-                price = 2.8;
-                break;
-            case "Narcissus":
-                price = 3.0;
-                break;
-            case "Gladiolus":
-                price = 2.5;
-                break;
-        }
+        double price = switch (flower) {
+            case "Roses" -> 5.0;
+            case "Dahlias" -> 3.8;
+            case "Tulips" -> 2.8;
+            case "Narcissus" -> 3.0;
+            case "Gladiolus" -> 2.5;
+            default -> 0;
+        };
         total = price * count;
         if(flower.equalsIgnoreCase("Roses")
         && (count > 80)){
             total = total - (total * 0.1);
         }
 
+        if(flower.equalsIgnoreCase("Dahlias")
+        && count > 90) {
+        total = total - (total * 0.15);
+        }
+
+        if(flower.equalsIgnoreCase("Tulips")
+                && count > 80) {
+            total = total - (total * 0.15);
+        }
+
+        if(flower.equalsIgnoreCase("Narcissus")
+                && count < 120) {
+            total = total + (total * 0.15);
+        }
+
+        if(flower.equalsIgnoreCase("Gladiolus")
+                && count < 80) {
+            total = total + (total * 0.2);
+        }
+
         if (budget >= total) {
             System.out.printf("Hey, you have a great " +
                             "garden with %d %s " +
                             "and %.2f leva left.",
-                    count, flower, change);
+                    count, flower, Math.abs(budget - total));
         } else {
             System.out.printf("Not enough money, you need " +
                     "%.2f leva more.", Math.abs(total - budget));
