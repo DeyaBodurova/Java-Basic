b317979d986527668a70f8bf8b5966fa3b6872f1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-21 18:00:55 +0300

    wip

--- src/NewHouse.java
+++ src/NewHouse.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class NewHouse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String flower = sc.nextLine();
+
+
     }
 }
