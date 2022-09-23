45eb4db29e6b62fca71f6cc71d1691695bfa9253
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-23 17:50:43 +0300

    wip

--- src/NewHouse.java
+++ src/NewHouse.java
@@ -14,6 +14,7 @@ public class NewHouse {
             case "Narcissus" -> 3.0;
             case "Gladiolus" -> 2.5;
             default -> 0;
+
         };
         total = price * count;
         if(flower.equalsIgnoreCase("Roses")
