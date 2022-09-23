ce15ddba55a69785a4b13a67e3f0dd9a7a1f4fe5
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-23 17:51:23 +0300

    wip

--- src/NewHouse.java
+++ src/NewHouse.java
@@ -20,7 +20,6 @@ public class NewHouse {
         && (count > 80)){
             total = total - (total * 0.1);
         }
-
         if(flower.equalsIgnoreCase("Dahlias")
         && count > 90) {
         total = total - (total * 0.15);
