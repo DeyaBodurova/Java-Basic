9c54ea88b9a301dbdf74166c9e513385c91e43da
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:50:35 +0300

    wip

---
+++ src/FruitShop.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class FruitShop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fruit = sc.nextLine();
+        String day = sc.nextLine();
+        double quantity = Double.parseDouble(sc.nextLine());
+        double sum = 0;
+        switch (day) {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                switch (fruit) {
+                    case "banana":
+                        sum = quantity * 2.5;
+                        break;
+                    case "apple":
+                        sum = quantity * 1.2;
+                        break;
+                    case "orange":
+                        sum = quantity * 0.85;
+                        break;
+                    case "grapefruit":
+                        sum = quantity * 1.45;
+                        break;
+                    case "kiwi":
+                        sum = quantity * 2.7;
+                        break;
+                    case "pineapple":
+                        sum = quantity *5.5;
+                        break;
+                    case "grapes":
+                        sum = quantity *3.85;
+                        break;
+                    default:
+                        System.out.println("error");
+                        break;
+                }
+                break;
+            case "Saturday":
+            case "Sunday":
+                switch (fruit) {
+                    case "banana":
+                        sum = quantity * 2.7;
+                        break;
+                    case "apple":
+                        sum = quantity * 1.25;
+                        break;
+                    case "orange":
+                        sum = quantity * 0.9;
+                        break;
+                    case "grapefruit":
+                        sum = quantity * 1.6;
+                        break;
+                    case "kiwi":
+                        sum = quantity * 3.0;
+                        break;
+                    case "pineapple":
+                        sum = quantity * 5.6;
+                        break;
+                    case "grapes":
+                        sum = quantity * 4.2;
+                        break;
+                    default:
+                        System.out.println("error");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("error");
+        }
+    }
+}
