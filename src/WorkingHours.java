2306da59639fe1fb65e31ef01863660a233e9700
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:26:45 +0300

    wip

--- src/WorkingHours.java
+++ src/WorkingHours.java
@@ -5,6 +5,24 @@ public class WorkingHours {
         Scanner sc = new Scanner(System.in);
         int hour = Integer.parseInt(sc.nextLine());
         String day = sc.nextLine();
-
+        if (hour >= 10 & hour <=18) {
+            switch (day){
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    System.out.println("open");
+                    break;
+                case "Sunday":
+                    System.out.println("closed");
+                    break;
+                default:
+                    System.out.println("Invalid day!");
+            }
+        } else {
+            System.out.println("closed");
+        }
     }
 }
