4c0532cb2fe47f38c900fed6dd006d899d0c9b76
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:22:56 +0300

    wip

--- src/WorkingHours.java
+++ src/WorkingHours.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class WorkingHours {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int hour = Integer.parseInt(sc.nextLine());
+        String day = sc.nextLine();
 
     }
 }
