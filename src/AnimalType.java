253005e3cd6a239554ad224e79cb4d292fb936a2
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:02:10 +0300

    wip

--- src/AnimalType.java
+++ src/AnimalType.java
@@ -4,5 +4,10 @@ public class AnimalType {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String animal = sc.nextLine();
+        switch (animal){
+            case "dog":
+                System.out.println("mammal");
+                break;
+        }
     }
 }
