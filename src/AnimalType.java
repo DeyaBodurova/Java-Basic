209dea3e47775402f3ac3046fcb8496a1da6cca6
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:01:40 +0300

    wip

--- src/AnimalType.java
+++ src/AnimalType.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class AnimalType {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String animal = sc.nextLine();
     }
 }
