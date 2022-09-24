7bd7ca20213afa3e595eb88fcb37d82dd7c4b6da
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:11:05 +0300

    wip

--- src/SmallShop.java
+++ src/SmallShop.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class SmallShop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String product = sc.nextLine();
+        String city = sc.nextLine();
+        double quantity = Double.parseDouble(sc.nextLine());
+
     }
 }
