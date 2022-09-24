6ec8ec8f5749b11a4a00562933f5691489bd9be9
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:05:47 +0300

    wip

--- src/PersonalTitles.java
+++ src/PersonalTitles.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PersonalTitles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        double age = Double.parseDouble(sc.nextLine());
+        String gender = sc.nextLine();
     }
 }
