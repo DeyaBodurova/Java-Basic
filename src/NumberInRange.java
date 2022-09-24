8f89327afcece6185a5f60474771c3e8885d2708
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:20:59 +0300

    wip

--- src/NumberInRange.java
+++ src/NumberInRange.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class NumberInRange {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int n = Integer.parseInt(sc.nextLine());
+        if(n <= 100 & n >= -100 & n != 0){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
