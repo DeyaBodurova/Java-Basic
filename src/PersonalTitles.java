8bc20bd694c71594e640fb751b40f236fd519c47
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-24 14:08:05 +0300

    wip

--- src/PersonalTitles.java
+++ src/PersonalTitles.java
@@ -5,5 +5,24 @@ public class PersonalTitles {
         Scanner sc = new Scanner(System.in);
         double age = Double.parseDouble(sc.nextLine());
         String gender = sc.nextLine();
+        if(age < 16){
+            switch (gender){
+                case "m":
+                    System.out.println("Master");
+                    break;
+                case "f":
+                    System.out.println("Miss");
+                    break;
+            }
+        } else {
+            switch (gender){
+                case "m":
+                    System.out.println("Mr.");
+                    break;
+                case "f":
+                    System.out.println("Ms.");
+                    break;
+            }
+        }
     }
 }
