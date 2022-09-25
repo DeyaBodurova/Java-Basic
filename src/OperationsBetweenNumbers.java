02e0b3ac7b89e9ddaac3e7be7475b0a88d7de5ff
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 08:37:03 +0300

    wip

--- src/OperationsBetweenNumbers.java
+++ src/OperationsBetweenNumbers.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class OperationsBetweenNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.nextLine());
+        int b = Integer.parseInt(sc.nextLine());
+
     }
 }
