d964bbde78e866c9367591b524041cf18a4bfe5d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 08:19:31 +0300

    wip

--- src/Journey.java
+++ src/Journey.java
@@ -5,6 +5,17 @@ public class Journey {
         Scanner sc = new Scanner(System.in);
         double budget = Double.parseDouble(sc.nextLine());
         String season = sc.nextLine();
+        String type;
+        switch (season) {
+            case "summer":
 
+                break;
+            case "winter":
+                break;
+        }
+
+        System.out.printf("Somewhere in %s\n", destination);
+        System.out.printf("%s – %.2f", type,
+                moneySpend);
     }
 }
