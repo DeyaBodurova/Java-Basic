fda0d75687fd7b43d552436011306d335bc39bbb
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 07:55:00 +0300

    wip

--- src/FishingBoat.java
+++ src/FishingBoat.java
@@ -21,21 +21,21 @@ public class FishingBoat {
         }
 
         if (fisherman <= 6) {
-            boatPrice = boatPrice - (boatPrice*0.1);
-        } else if(fisherman>7 & fisherman<=11){
+            boatPrice = boatPrice - (boatPrice * 0.1);
+        } else if (fisherman > 6 & fisherman <= 11) {
             boatPrice = boatPrice - (boatPrice * 0.15);
-        } else if (fisherman > 12){
-            boatPrice = boatPrice - (boatPrice*0.25);
+        } else if (fisherman >= 12) {
+            boatPrice = boatPrice - (boatPrice * 0.25);
         }
 
-        if(fisherman % 2 == 0 & (!season.equals("Autumn"))){
+        if (fisherman % 2 == 0 & (!season.equals("Autumn"))) {
             boatPrice -= boatPrice * 0.5;
         }
 
-        if(budget >= boatPrice){
-            System.out.printf("Yes! You have %.2f leva left.", Math.abs(budget-boatPrice));
+        if (budget >= boatPrice) {
+            System.out.printf("Yes! You have %.2f leva left.", Math.abs(budget - boatPrice));
         } else {
-            System.out.printf("Not enough money! You need %.2f leva.",Math.abs(boatPrice - budget));
+            System.out.printf("Not enough money! You need %.2f leva.", Math.abs(boatPrice - budget));
         }
     }
 }
