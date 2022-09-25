19940cc23f03f58847107e1ece10923da72a62e0
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 10:52:31 +0300

    wip

--- src/SkiTrip.java
+++ src/SkiTrip.java
@@ -10,35 +10,34 @@ public class SkiTrip {
         double discount = 0;
         switch (typeRoom) {
             case "room for one person":
-                price = (days -1) * 18.0;
+                price = (days - 1) * 18.0;
                 break;
             case "apartment":
-                price = (days -1) * 25.0;
-                if(days < 10){
-                    price -= price*0.3;
-                } else if(days <=15){
-                    price -= price*0.35;
+                price = (days - 1) * 25.0;
+                if (days < 10) {
+                    price -= price * 0.3;
+                } else if (days <= 15) {
+                    price -= price * 0.35;
                 } else {
                     price -= price * 0.5;
                 }
                 break;
             case "president apartment":
-                price = (days -1) * 35.0;
-                if(days < 10){
-                    price -= price*0.1;
-                } else if(days <=15){
-                    price -= price*0.15;
+                price = (days - 1) * 35.0;
+                if (days < 10) {
+                    price -= price * 0.1;
+                } else if (days <= 15) {
+                    price -= price * 0.15;
                 } else {
                     price -= price * 0.2;
                 }
                 break;
         }
-        if(grade.equals("positive")){
+        if (grade.equals("positive")) {
             price += price * 0.25;
         } else {
             price -= price * 0.1;
         }
-
-        System.out.printf("%.2f",price);
+        System.out.printf("%.2f", price);
     }
 }
