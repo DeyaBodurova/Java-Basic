099b7bf5b5312af28b279e7114c1c38fa3d72dbb
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 08:14:01 +0300

    wip

---
+++ src/Journey.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Journey {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double budget = Double.parseDouble(sc.nextLine());
+        String season = sc.nextLine();
+
+    }
+}
