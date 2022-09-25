29435db27b70c47292ae98360636a4d471ff5b20
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 08:44:57 +0300

    wip

--- src/OperationsBetweenNumbers.java
+++ src/OperationsBetweenNumbers.java
@@ -6,6 +6,29 @@ public class OperationsBetweenNumbers {
         int a = Integer.parseInt(sc.nextLine());
         int b = Integer.parseInt(sc.nextLine());
         String operator = sc.nextLine();
+        double result = 0.0;
+        boolean isPossible = true;
+        switch (operator) {
+            case "+":
+                if ((a + b) % 2 == 0) {
+                    System.out.println("%d + %d = %d - even");
+                } else {
+                    System.out.println("%d + %d = %d - odd");
+                }
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                if (b == 0) {
+                    System.out.printf("Cannot divide %d by zero", a);
+                    break;
+                }
 
+                break;
+            case "%":
+                break;
+        }
     }
 }
