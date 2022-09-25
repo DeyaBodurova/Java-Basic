93e57aa35c526521b86e2e2f5ad09dadd9830583
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 09:14:04 +0300

    wip

---
+++ src/HotelRoom.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class HotelRoom {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String month = sc.nextLine();
+        int nights = Integer.parseInt(sc.nextLine());
+        double priceStudio = 0.0;
+        double priceApartment = 0.0;
+        switch (month) {
+            case "May":
+            case "October":
+                priceStudio = 50 * nights;
+                priceApartment = 65 * nights;
+                break;
+            case "June":
+            case "September":
+                priceStudio = 75.2 * nights;
+                priceApartment = 68.7 * nights;
+                break;
+            case "July":
+            case "August":
+                priceStudio = 76 * nights;
+                priceApartment = 77 * nights;
+                break;
+        }
+        if(nights > 14 && ((month.equals("May")) ||
+                (month.equals("October")))){
+            priceStudio -= priceStudio * 0.3;
+        } else if(nights > 14 && ((month.equals("June")) ||
+                (month.equals("September")))) {
+            priceStudio -= priceStudio * 0.2;
+        } else if(nights > 7 && ((month.equals("May")) ||
+                (month.equals("October")))) {
+            priceStudio -= priceStudio * 0.05;
+        } else if(nights > 14) {
+            priceApartment -= priceApartment * 0.1;
+        }
+
+        System.out.printf("Apartment: %.2f lv\n", priceApartment);
+        System.out.printf("Studio: %.2f lv\n", priceStudio);
+    }
+}
