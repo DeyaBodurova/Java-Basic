4bd8882d428b1711e3c440c50b209a067f073876
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 10:52:00 +0300

    wip

---
+++ src/SkiTrip.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SkiTrip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int days = Integer.parseInt(sc.nextLine());
+        String typeRoom = sc.nextLine();
+        String grade = sc.nextLine();
+        double price = 0.0;
+        double discount = 0;
+        switch (typeRoom) {
+            case "room for one person":
+                price = (days -1) * 18.0;
+                break;
+            case "apartment":
+                price = (days -1) * 25.0;
+                if(days < 10){
+                    price -= price*0.3;
+                } else if(days <=15){
+                    price -= price*0.35;
+                } else {
+                    price -= price * 0.5;
+                }
+                break;
+            case "president apartment":
+                price = (days -1) * 35.0;
+                if(days < 10){
+                    price -= price*0.1;
+                } else if(days <=15){
+                    price -= price*0.15;
+                } else {
+                    price -= price * 0.2;
+                }
+                break;
+        }
+        if(grade.equals("positive")){
+            price += price * 0.25;
+        } else {
+            price -= price * 0.1;
+        }
+
+        System.out.printf("%.2f",price);
+    }
+}
