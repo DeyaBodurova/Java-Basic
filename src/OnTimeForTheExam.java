edbff6f0824195e70eb2fd71c01ddad531ffc751
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-25 10:08:13 +0300

    wip

--- src/OnTimeForTheExam.java
+++ src/OnTimeForTheExam.java
@@ -11,8 +11,8 @@ public class OnTimeForTheExam {
         int totalMinExam = hourExam * 60 + minutesExam;
         int totalMinArrival = hourArrival * 60 + minutesArrival;
 
-        int difference=0;
-        if(totalMinArrival < totalMinExam) {
+        int difference = 0;
+        if (totalMinArrival < (totalMinExam - 30)) {
             System.out.println("Early");
             difference = totalMinExam - totalMinArrival;
             if (difference <= 60) {
@@ -22,6 +22,25 @@ public class OnTimeForTheExam {
                 min = difference % 60;
                 difference = difference / 60;
                 System.out.printf("%d:%d hours before the start",
+                        difference, min);
+            }
+        } else if (((totalMinExam - 30) <= totalMinArrival) &&
+                totalMinArrival != totalMinExam) {
+            System.out.println("On time");
+            if(totalMinArrival != totalMinExam) {
+                int min = totalMinExam - totalMinArrival;
+                System.out.printf("%d minutes before the start", min);
+            }
+        } else if(totalMinArrival > totalMinExam) {
+            System.out.println("Late");
+            difference = totalMinArrival - totalMinExam;
+            if(difference <=60) {
+                System.out.printf("0:%d hours after the start",difference);
+            } else {
+                int min = 0;
+                min = difference %60;
+                difference = difference /60;
+                System.out.printf("%d:%d hours after the start",
                         difference,min);
             }
         }
