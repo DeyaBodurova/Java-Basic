1ab3fd9d030fefe236ae5affbf85e2f9f151ed83
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-26 07:47:59 +0300

    wip

--- src/Num1To100.java
+++ src/Num1To100.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Num1To100 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i+1);
+        }
     }
 }
