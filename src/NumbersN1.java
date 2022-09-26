972754529fc34d4e99b4d396e9c186464a24bf21
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-26 07:51:13 +0300

    wip

--- src/NumbersN1.java
+++ src/NumbersN1.java
@@ -4,6 +4,8 @@ public class NumbersN1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
     }
 }
