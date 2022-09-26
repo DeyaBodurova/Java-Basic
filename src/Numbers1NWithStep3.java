ae9f9c6dcf8008a490652fe74421bbb7893a866b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-26 07:56:51 +0300

    wip

--- src/Numbers1NWithStep3.java
+++ src/Numbers1NWithStep3.java
@@ -4,6 +4,8 @@ public class Numbers1NWithStep3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-
+        for (int i = 1; i <=n; i+=3) {
+            System.out.println(i);
+        }
     }
 }
