2c9b0392aec7b7d7431d92c3ecc0d2c8a17327bb
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-26 19:52:13 +0300

    wip

--- src/VowelsSum.java
+++ src/VowelsSum.java
@@ -1,7 +1,24 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class VowelsSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if(text.toLowerCase().charAt(i) == 'a'){
+                count += 1;
+            } else if (text.toLowerCase().charAt(i) == 'e') {
+                count += 2;
+            } else if(text.toLowerCase().charAt(i) == 'i'){
+                count += 3;
+            } else if(text.toLowerCase().charAt(i) == 'o'){
+                count += 4;
+            } else if(text.toLowerCase().charAt(i) == 'u'){
+                count += 5;
+            }
+        }
+        System.out.println(count);
     }
 }
