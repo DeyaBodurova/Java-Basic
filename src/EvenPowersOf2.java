121d347a7841a86be51fdb0136a40120728c14e7
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-26 18:29:09 +0300

    wip

--- src/EvenPowersOf2.java
+++ src/EvenPowersOf2.java
@@ -4,8 +4,10 @@ public class EvenPowersOf2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 0; i < n; i++) {
+            if(i%2==0) {
+                System.out.println((int)Math.pow(2,i));
+            }
         }
     }
 }
