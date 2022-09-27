0afb1de73e529c741d6d152e3982539553eb6f51
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-27 07:49:38 +0300

    wip

--- src/NumberSequence.java
+++ src/NumberSequence.java
@@ -4,6 +4,18 @@ public class NumberSequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = Integer.parseInt(sc.nextLine());
-
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < count; i++) {
+            int n = Integer.parseInt(sc.nextLine());
+            if(min <= n) {
+                min = n;
+            }
+            if(max >= n){
+                max = n;
+            }
+        }
+        System.out.println("Max number: "+ max);
+        System.out.println("Min nimber: "+ min);
     }
 }
