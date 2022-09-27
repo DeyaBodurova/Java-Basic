aea5c5507cc8d652f614cf48462d18c306850a07
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-27 07:46:39 +0300

    wip

--- src/NumberSequence.java
+++ src/NumberSequence.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class NumberSequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int count = Integer.parseInt(sc.nextLine());
 
     }
 }
