1201bb6be43522cfc412ae6d7a40d94a15bdcca5
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-27 19:02:29 +0300

    wip

--- src/OddEvenSum.java
+++ src/OddEvenSum.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class OddEvenSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int count =Integer.parseInt(sc.nextLine());
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int i = 0; i < count; i++) {
+            int n = Integer.parseInt(sc.nextLine());
+            if(i%2==0){
+                evenSum+=n;
+            }else {
+                oddSum+=n;
+            }
+        }
     }
 }
