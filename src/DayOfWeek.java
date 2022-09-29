60649971641c8e2a7d51fb94d982de72dae7e29f
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-29 21:58:16 +0300

    wip

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -5,29 +5,14 @@ public class DayOfWeek {
         Scanner sc = new Scanner(System.in);
         int num = Integer.parseInt(sc.nextLine());
         switch (num) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Error");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Error");
         }
     }
 }
