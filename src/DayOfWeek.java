c7b42265da1d91e611066bb909090d90a34f092d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-29 21:57:55 +0300

    wip

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -4,7 +4,7 @@ public class DayOfWeek {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = Integer.parseInt(sc.nextLine());
-        switch (num){
+        switch (num) {
             case 1:
                 System.out.println("Monday");
                 break;
