06f90d0d46e147c8a5e2245302a8356098dd9ac6
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-30 22:00:41 +0300

    wip

--- src/InvalidNumber.java
+++ src/InvalidNumber.java
@@ -4,7 +4,7 @@ public class InvalidNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = Integer.parseInt(sc.nextLine());
-        if(number != 0 & (number < 100 || number > 200)){
+        if (number != 0 & (number < 100 || number > 200)) {
             System.out.println("invalid");
         }
     }
