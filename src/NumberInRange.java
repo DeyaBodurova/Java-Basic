758dc0377fbde6e74f7f57d63ee8d6bafacf4106
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-30 22:21:13 +0300

    wip

--- src/NumberInRange.java
+++ src/NumberInRange.java
@@ -4,7 +4,7 @@ public class NumberInRange {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        if(n <= 100 & n >= -100 & n != 0){
+        if (n <= 100 & n >= -100 & n != 0) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
