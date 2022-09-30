d7e976fc8d9bf6d0a5bc2ee07698c9496dc0a5ba
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-09-30 22:04:12 +0300

    wip

--- src/LeftAndRightSum.java
+++ src/LeftAndRightSum.java
@@ -6,17 +6,17 @@ public class LeftAndRightSum {
         int count = Integer.parseInt(sc.nextLine());
         int leftSum = 0;
         int rightSum = 0;
-        for (int i = 1; i <= 2*count; i++) {
+        for (int i = 1; i <= 2 * count; i++) {
             int n = Integer.parseInt(sc.nextLine());
-            if(i <= count){
+            if (i <= count) {
                 leftSum += n;
             } else {
-                rightSum +=n;
+                rightSum += n;
             }
         }
 
-        if(leftSum == rightSum) {
-            System.out.printf("Yes, sum = %d",leftSum);
+        if (leftSum == rightSum) {
+            System.out.printf("Yes, sum = %d", leftSum);
         } else {
             System.out.printf("No, diff = %d",
                     Math.abs(leftSum - rightSum));
