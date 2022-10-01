c9fb9444af739dd7d4d252d2d022f63c81041a0b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-01 22:18:52 +0300

    wip

--- src/CleverLily.java
+++ src/CleverLily.java
@@ -4,6 +4,7 @@ public class CleverLily {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = Integer.parseInt(sc.nextLine());
-
+        double washingMachinePrice = Double.parseDouble(sc.nextLine());
+        int toyPrice = Integer.parseInt(sc.nextLine());
     }
 }
