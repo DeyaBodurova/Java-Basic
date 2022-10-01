24fcceba5fb5e50bf4882b82052893ab769bf7ae
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-01 21:11:28 +0300

    wip

--- src/HalfSumElement.java
+++ src/HalfSumElement.java
@@ -4,5 +4,22 @@ public class HalfSumElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = Integer.parseInt(sc.nextLine());
+        int maxNum = 0;
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            int a = Integer.parseInt(sc.nextLine());
+            if (maxNum <= a) {
+                sum += maxNum;
+                maxNum = a;
+            } else {
+                sum += a;
+            }
+        }
+        if (sum == maxNum) {
+            System.out.println("Yes\nSum = " + sum);
+        } else {
+            System.out.println("No\nDiff = " +
+                    Math.abs(maxNum - sum));
+        }
     }
 }
