f24dc8c9cb0db814194e72ddba3d8ae3fffc8efa
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-01 21:04:17 +0300

    wip

--- src/HalfSumElement.java
+++ src/HalfSumElement.java
@@ -4,23 +4,5 @@ public class HalfSumElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = Integer.parseInt(sc.nextLine());
-
-        //todo to fix this mess - it's doesn't work!!
-        for (int i = 1; i <= count; i++) {
-            int n = Integer.parseInt(sc.nextLine());
-            int maxValue = n;
-            int sum = 0;
-            for (int j = 0; j < count - i; j++) {
-                int a = Integer.parseInt(sc.nextLine());
-                if (maxValue>a) {
-                    sum += a;
-                    maxValue = n;
-                } else {
-                    sum += n;
-                    maxValue = a;
-                }
-            }
-            System.out.println(sum);
-        }
     }
 }
