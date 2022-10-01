24952ad10883902c0e4fda27ea22de1001508638
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-01 21:54:36 +0300

    wip

--- src/Histogram.java
+++ src/Histogram.java
@@ -4,11 +4,11 @@ public class Histogram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        int under200 = 0;
-        int under399 = 0;
-        int under599 = 0;
-        int under799 = 0;
-        int above800 = 0;
+        double under200 = 0;
+        double under399 = 0;
+        double under599 = 0;
+        double under799 = 0;
+        double above800 = 0;
         for (int i = 0; i < n; i++) {
             int a = Integer.parseInt(sc.nextLine());
             if (a < 200) {
@@ -23,31 +23,7 @@ public class Histogram {
                 above800++;
             }
         }
-        if(under200==0){
-            System.out.printf("0.00%\n");
 
-        }else{
-            System.out.printf("%.2f%\n",(under200/n*100));
-        }
-        if(){
-            System.out.printf("0.00%\n");
-        }else{
-            System.out.printf("%.2f\n",under399/n*100);
-        }
-        if(){
-            System.out.printf("0.00%\n");
-        }else{
-            System.out.printf("%.2f\n",under599/n*100);
-        }
-        if(under799==0){
-            System.out.print("0.00%\n");
-        }else{
-            System.out.printf("%.2f\n",under799/n*100);
-        }
-        if(above800==0){
-            System.out.print("0.00%\n");
-        }else{
-            System.out.printf("%.2f\n",above800/n*100);
-        }
+
     }
 }
