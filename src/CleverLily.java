2ba03fb2db623d1e5125009be0d19199e8995c1a
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-01 22:17:48 +0300

    wip

--- src/CleverLily.java
+++ src/CleverLily.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CleverLily {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int age = Integer.parseInt(sc.nextLine());
 
     }
 }
