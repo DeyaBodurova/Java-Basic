64fcc6189744e16d5e29346fa9ccfaa309f04e68
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-01 22:23:48 +0300

    wip

--- src/CleverLily.java
+++ src/CleverLily.java
@@ -6,5 +6,14 @@ public class CleverLily {
         int age = Integer.parseInt(sc.nextLine());
         double washingMachinePrice = Double.parseDouble(sc.nextLine());
         int toyPrice = Integer.parseInt(sc.nextLine());
+        int toysCounter = 0;
+        int birthdayMoney = 0;
+        for (int i = 1; i <= age; i++) {
+            if (i % 2 == 0) {
+                birthdayMoney += (i * 5)-1;
+            } else {
+                toysCounter++;
+            }
+        }
     }
 }
