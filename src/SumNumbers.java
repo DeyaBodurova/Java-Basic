2167994b9200d0219798a8c91ef94ad4cb45b297
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-02 11:03:26 +0300

    wip

--- src/SumNumbers.java
+++ src/SumNumbers.java
@@ -4,10 +4,10 @@ public class SumNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = Integer.parseInt(sc.nextLine());
-        int sum =0;
+        int sum = 0;
         for (int i = 0; i < count; i++) {
             int n = Integer.parseInt(sc.nextLine());
-            sum+=n;
+            sum += n;
         }
         System.out.println(sum);
     }
