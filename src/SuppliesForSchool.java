14f43c9f491a7d4234acef65c20f11b052d35fdb
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-02 11:02:17 +0300

    wip

--- src/SuppliesForSchool.java
+++ src/SuppliesForSchool.java
@@ -7,7 +7,6 @@ public class SuppliesForSchool {
         int markers = Integer.parseInt(sc.nextLine());
         int cleaningSupplies = Integer.parseInt(sc.nextLine());
         int discount = Integer.parseInt(sc.nextLine());
-
         double sum = pens * 5.80 + (markers * 7.2) + (cleaningSupplies * 1.2);
         double finalSum = sum - (sum * (discount * 0.01));
         System.out.println(finalSum);
