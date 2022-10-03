042a7934916aedc0995787b0744350d536212c9d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-03 18:47:59 +0300

    wip

--- src/SmallShop.java
+++ src/SmallShop.java
@@ -7,36 +7,32 @@ public class SmallShop {
         String city = sc.nextLine();
         double quantity = Double.parseDouble(sc.nextLine());
         double sum = 0;
-        switch (city) {
-            case "Sofia":
-                switch (product) {
-                    case "coffee" -> sum = quantity * 0.5;
-                    case "water" -> sum = quantity * 0.8;
-                    case "beer" -> sum = quantity * 1.2;
-                    case "sweets" -> sum = quantity * 1.45;
-                    case "peanuts" -> sum = quantity * 1.6;
-                }
-                break;
-            case "Plovdiv":
-                sum = switch (product) {
-                    case "coffee" -> quantity * 0.4;
-                    case "water" -> quantity * 0.7;
-                    case "beer" -> quantity * 1.15;
-                    case "sweets" -> quantity * 1.30;
-                    case "peanuts" -> quantity * 1.5;
-                    default -> sum;
-                };
-                break;
-            case "Varna":
-                sum = switch (product) {
-                    case "coffee" -> quantity * 0.45;
-                    case "water" -> quantity * 0.7;
-                    case "beer" -> quantity * 1.1;
-                    case "sweets" -> quantity * 1.35;
-                    case "peanuts" -> quantity * 1.55;
-                    default -> sum;
-                };
-                break;
+        if ("Sofia".equals(city)) {
+            switch (product) {
+                case "coffee" -> sum = quantity * 0.5;
+                case "water" -> sum = quantity * 0.8;
+                case "beer" -> sum = quantity * 1.2;
+                case "sweets" -> sum = quantity * 1.45;
+                case "peanuts" -> sum = quantity * 1.6;
+            }
+        } else if ("Plovdiv".equals(city)) {
+            sum = switch (product) {
+                case "coffee" -> quantity * 0.4;
+                case "water" -> quantity * 0.7;
+                case "beer" -> quantity * 1.15;
+                case "sweets" -> quantity * 1.30;
+                case "peanuts" -> quantity * 1.5;
+                default -> sum;
+            };
+        } else if ("Varna".equals(city)) {
+            sum = switch (product) {
+                case "coffee" -> quantity * 0.45;
+                case "water" -> quantity * 0.7;
+                case "beer" -> quantity * 1.1;
+                case "sweets" -> quantity * 1.35;
+                case "peanuts" -> quantity * 1.55;
+                default -> sum;
+            };
         }
         System.out.printf("%.2f", sum);
     }
