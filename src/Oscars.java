24fe4c31df01826ff619c14ac639b86a53793dd3
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-04 08:07:05 +0300

    wip

--- src/Oscars.java
+++ src/Oscars.java
@@ -21,5 +21,9 @@ public class Oscars {
             }
 
         }
+        if(actorsPoints < 1250.5){
+            System.out.printf("Sorry, %s you need %.2f more!",
+                    name, Math.abs(1250.5-actorsPoints));
+        }
     }
 }
