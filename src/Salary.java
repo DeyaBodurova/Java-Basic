415f66b8a1b5e830a7ad2d9f9875a59275138be4
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-04 07:49:49 +0300

    wip

--- src/Salary.java
+++ src/Salary.java
@@ -4,6 +4,8 @@ public class Salary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int openTabs = Integer.parseInt(sc.nextLine());
+        double salary = Double.parseDouble(sc.nextLine());
+
 
     }
 }
