928f6aeb324196db8ee1c3948efc29f970ccdd54
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-05 20:58:49 +0300

    wip

--- src/Repainting.java
+++ src/Repainting.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Repainting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int naylon = Integer.parseInt(sc.nextLine());
+        int nylon = Integer.parseInt(sc.nextLine());
         int paint = Integer.parseInt(sc.nextLine());
         int thinner = Integer.parseInt(sc.nextLine());
         int hours = Integer.parseInt(sc.nextLine());
-        double nylonSum = (naylon + 2) * 1.5;
+        double nylonSum = (nylon + 2) * 1.5;
         double paintSum = 14.5 * (paint + (paint * 0.1));
         double thinnerSum = thinner * 5.0;
         double sumSupplies = nylonSum + paintSum + thinnerSum + 0.4;
