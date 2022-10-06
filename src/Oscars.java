6dba4f78b1ae4e5070d4a85a784c26fa5beb1550
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-06 18:34:14 +0300

    wip

--- src/Oscars.java
+++ src/Oscars.java
@@ -7,7 +7,6 @@ public class Oscars {
         double actorsPoints = Double.parseDouble(sc.nextLine());
         int count = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < count; i++) {
-
             String nameFromCommission = sc.nextLine();
             double points = Double.parseDouble(sc.nextLine());
             double sumPoints = (nameFromCommission.length() * points) / 2;
