d09c3b703f646609e45a1ce10b073883a457fc96
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-07 22:08:03 +0300

    wip

--- src/testy.java
+++ src/testy.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class testy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String actorName= sc.nextLine();
+        double academyPoints = Double.parseDouble(sc.nextLine());
+        int jury = Integer.parseInt(sc.nextLine());
 
+        for (int i = 0; i < jury; i++) {
+            String judgeName sc.nextLine();
+            double juryPoints = Double.parseDouble(sc.nextLine());
+
+        }
     }
 }
