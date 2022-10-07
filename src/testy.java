4d7610598c0de3079ad172f13cc7095830df2db2
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-07 22:14:57 +0300

    wip

--- src/testy.java
+++ src/testy.java
@@ -6,11 +6,24 @@ public class testy {
         String actorName= sc.nextLine();
         double academyPoints = Double.parseDouble(sc.nextLine());
         int jury = Integer.parseInt(sc.nextLine());
+        boolean isSucceeded = false;
 
         for (int i = 0; i < jury; i++) {
             String judgeName =sc.nextLine();
             double juryPoints = Double.parseDouble(sc.nextLine());
-
+            academyPoints += (judgeName.length() * juryPoints)/2;
+            if(academyPoints > 1250.5){
+                isSucceeded = true;
+                return;
+            }
+        }
+        if(isSucceeded){
+            System.out.printf("Congratulations, %s got a nominee" +
+                    " for leading role with %.0f!", actorName,
+                   academyPoints);
+        } else {
+            System.out.printf("Sorry, %s you need %.0f more!",
+                    actorName, 1250.5 - academyPoints);
         }
     }
 }
