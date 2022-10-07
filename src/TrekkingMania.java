353a54c6dd4b9e3f05974e626d5d6570495dcd32
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-07 22:28:20 +0300

    wip

--- src/TrekkingMania.java
+++ src/TrekkingMania.java
@@ -4,9 +4,16 @@ public class TrekkingMania {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int groups = Integer.parseInt(sc.nextLine());
+        double musalaPercent;
+        double monblanPercent;
+        double kilimangaroPercent;
+        double k2Percent;
+        double everestPercent;
         for (int i = 0; i < groups; i++) {
             int peopleInGroup = Integer.parseInt(sc.nextLine());
+            if(peopleInGroup <= 5){
 
+            }
         }
     }
 }
