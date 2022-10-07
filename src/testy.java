ae9361dc8bdd173675e19c0d9314185357f1280f
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-07 22:04:35 +0300

    wip

--- src/testy.java
+++ src/testy.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class testy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tabs = Integer.parseInt(sc.nextLine());
-        double salary = Double.parseDouble(sc.nextLine());
 
     }
 }
