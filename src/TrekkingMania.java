3fc7ef7d2d3cad9cf4c44d4dbea2c965f97a88cd
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-07 22:26:17 +0300

    wip

--- src/TrekkingMania.java
+++ src/TrekkingMania.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class TrekkingMania {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int groups = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < groups; i++) {
+            int peopleInGroup = Integer.parseInt(sc.nextLine());
 
+        }
     }
 }
