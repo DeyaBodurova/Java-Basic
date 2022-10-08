0fd3f822cd7d0b2e69f5a3c7e8a1efe2c7d92346
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-08 21:52:33 +0300

    wip

--- src/Oscars.java
+++ src/Oscars.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Oscars {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String actorName= sc.nextLine();
+        String actorName = sc.nextLine();
         double academyPoints = Double.parseDouble(sc.nextLine());
         int jury = Integer.parseInt(sc.nextLine());
         boolean isSucceeded = false;
 
         for (int i = 0; i < jury; i++) {
-            String judgeName =sc.nextLine();
+            String judgeName = sc.nextLine();
             double juryPoints = Double.parseDouble(sc.nextLine());
-            academyPoints += (judgeName.length() * juryPoints)/2;
-            if(academyPoints > 1250.5){
+            academyPoints += (judgeName.length() * juryPoints) / 2;
+            if (academyPoints > 1250.5) {
                 isSucceeded = true;
                 break;
             }
         }
-        if(isSucceeded){
+        if (isSucceeded) {
             System.out.printf("Congratulations, %s got a nominee" +
                             " for leading role with %.1f!", actorName,
                     academyPoints);
