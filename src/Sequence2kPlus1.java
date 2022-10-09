85c68a17baf7552c6ca3217ed2865ffb9c086aa3
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-09 09:42:59 +0300

    wip

--- src/Sequence2kPlus1.java
+++ src/Sequence2kPlus1.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Sequence2kPlus1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int num = Integer.parseInt(sc.nextLine());
+        int a = 1;
+        while (a<num){
+            System.out.println(a);
+            a=a*2+1;
+        }
     }
 }
