7660f079a67b366286ebb742959045cb1d543915
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-09 09:33:00 +0300

    wip

--- src/SumNumbersWhileLoop.java
+++ src/SumNumbersWhileLoop.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class SumNumbersWhileLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        while (sum>=num){
+            int a= Integer.parseInt(sc.nextLine());
+            sum+=a;
+        }
+        System.out.println(sum);
     }
 }
