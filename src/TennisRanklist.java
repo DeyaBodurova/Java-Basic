b6aa70079a7c35cab54d3b3b816f33d3864e541f
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-09 07:24:59 +0300

    wip

--- src/TennisRanklist.java
+++ src/TennisRanklist.java
@@ -3,5 +3,26 @@ import java.util.Scanner;
 public class TennisRanklist {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int tournaments = Integer.parseInt(sc.nextLine());
+        int beginingPoints = Integer.parseInt(sc.nextLine());
+        int points = beginingPoints;
+        for (int i = 0; i < tournaments; i++) {
+            String stage = sc.nextLine();
+            switch (stage){
+                case "W":
+                    points+=2000;
+                    break;
+                case "F":
+                    points+=1200;
+                    break;
+                case "SF":
+                    points+=720;
+                    break;
+            }
+        }
+        int avg = (points- beginingPoints)/tournaments;
+        System.out.printf("Final points: %d\n",points);
+        System.out.printf("Average points: %d\n",avg);
+        System.out.println();
     }
 }
