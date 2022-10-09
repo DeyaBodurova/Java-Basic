6346f8bbe678aaceaa73210917e7a4e148f347ba
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-09 09:25:22 +0300

    wip

--- src/Password.java
+++ src/Password.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Password {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String username = sc.nextLine();
+        String password = sc.nextLine();
+        String passwordAttempt = sc.nextLine();
+        while (!passwordAttempt.equals(password)) {
+            System.out.println("Wrong password! " +
+                    "Please try again!");
+        }
+    if(password.equals(passwordAttempt)){
+        System.out.printf("Welcome %s",username);
+    }
     }
 }
