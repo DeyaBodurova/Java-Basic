af214e5b8c9f679c8fd2af1a69d34876fa10d1c5
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-09 09:17:23 +0300

    wip

--- src/ReadText.java
+++ src/ReadText.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class ReadText {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        while (!text.equals("Stop")){
+            System.out.println(text);
+        }
     }
 }
