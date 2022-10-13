8506bcd5ee85d43c80555d07acc13cd0d24a6fc8
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-13 21:30:48 +0300

    wip

--- src/AccountBalance.java
+++ src/AccountBalance.java
@@ -8,14 +8,14 @@ public class AccountBalance {
         while (!text.equals("NoMoreMoney")) {
             double deposition = Double.parseDouble(text);
             if (deposition >= 0) {
-                System.out.printf("Increase: %.2f\n",deposition);
-            money+=deposition;
+                System.out.printf("Increase: %.2f\n", deposition);
+                money += deposition;
             } else {
                 System.out.println("Invalid operation!");
                 break;
             }
             text = sc.nextLine();
         }
-        System.out.printf("Total: %.2f",money);
+        System.out.printf("Total: %.2f", money);
     }
 }
