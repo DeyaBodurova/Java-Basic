bb5be7e78b47de69b344ade6323b0f7e51be9413
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 11:22:20 +0300

    wip

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class MaxNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-     String input = sc.nextLine();
+        String input = sc.nextLine();
         int maxNum = Integer.MIN_VALUE;
-     while (!input.equals("Stop")){
+        while (!input.equals("Stop")) {
             int currentNum = Integer.parseInt(input);
-            if(currentNum>maxNum){
-                maxNum =currentNum;
+            if (currentNum > maxNum) {
+                maxNum = currentNum;
             }
-         input=sc.nextLine();
-     }
+            input = sc.nextLine();
+        }
         System.out.println(maxNum);
     }
 }
