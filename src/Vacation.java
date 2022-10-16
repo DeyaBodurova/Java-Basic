1602499589f120be4e92b6f6025720f9dee8ff78
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 11:26:03 +0300

    wip

--- src/Vacation.java
+++ src/Vacation.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
 public class Vacation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double moneyNeeded = Double.parseDouble(sc.nextLine());
+        double money = Double.parseDouble(sc.nextLine());
+        while () {
+            String action = sc.nextLine();
+            double saveSpend = Double.parseDouble(sc.nextLine());
+
+
+        }
     }
 }
