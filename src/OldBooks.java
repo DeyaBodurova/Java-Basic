7ef88d8a76ea78ea5cd96544cba9bc7b56b379b7
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 11:01:36 +0300

    wip

---
+++ src/OldBooks.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class OldBooks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        String input = sc.nextLine();
+        int countBooks = 1;
+        boolean isFound = false;
+        while (!input.equals("No More Books")) {
+            if (input.equals(title)) {
+                isFound = true;
+                break;
+            }
+            countBooks++;
+            input = sc.nextLine();
+        }
+        if (!isFound) {
+            System.out.println("The book you search is not here!");
+            System.out.printf("You checked %d books.", countBooks);
+        } else {
+            System.out.printf("You checked %d books and " +
+                    "found it.", countBooks);
+        }
+    }
+}
