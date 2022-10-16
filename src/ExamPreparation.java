16fb78e57f04f4d810df0a71c93aad300a643bd1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 11:18:01 +0300

    wip

---
+++ src/ExamPreparation.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ExamPreparation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int weekGrades = Integer.parseInt(sc.nextLine());
+        int countPoorGrades =0;
+        int countTasks =0;
+        int totalGrades=0;
+        boolean isFailed = false;
+        String input = sc.nextLine();
+        String taskName ="";
+        while (!input.equals("Enough")) {
+            taskName = input;
+            int currentGrade = Integer.parseInt(sc.nextLine());
+            totalGrades+=currentGrade;
+            countTasks++;
+            if (currentGrade <= 4) {
+                countPoorGrades++;
+                if (weekGrades == countPoorGrades) {
+                    isFailed=true;
+                    System.out.println("You need a break, "
+                    +weekGrades+ " poor grades.");
+                    break;
+                }
+            }
+            input = sc.nextLine();
+        }
+        if (!isFailed) {
+            System.out.println("Average score: " +totalGrades/countTasks);
+            System.out.println("Number of problems: " +countTasks);
+            System.out.println("Last problem: " +taskName);
+        }
+    }
+}
