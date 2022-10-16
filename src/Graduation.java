09acdad28b0f9d1441c509d35066f29837287b2d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 10:44:04 +0300

    wip

---
+++ src/Graduation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Graduation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int countClass = 0;
+        double yearGrade = 0;
+        int fails = 0;
+        while (countClass < 12){
+            double currentGrade = Integer.parseInt(sc.nextLine());
+            if(currentGrade < 4) {
+                fails++;
+                if(fails>1){
+                    System.out.printf("%s has been excluded at" +
+                            " %d grade",name,countClass);
+                    break;
+                }
+                continue;
+            }
+            countClass++;
+            yearGrade +=currentGrade;
+        }
+        if(countClass==12){
+            System.out.printf("%s graduated. Average grade: %.2f",
+                    name, yearGrade/12);
+        }
+    }
+}
