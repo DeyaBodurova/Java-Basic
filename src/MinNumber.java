ccd3af60d2b4edb1af79d198370220ed03e8309d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 11:22:12 +0300

    wip

--- src/MinNumber.java
+++ src/MinNumber.java
@@ -5,12 +5,12 @@ public class MinNumber {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         int minNum = Integer.MAX_VALUE;
-        while (!input.equals("Stop")){
-            int current=Integer.parseInt(input);
-            if(current<minNum){
-                minNum=current;
+        while (!input.equals("Stop")) {
+            int current = Integer.parseInt(input);
+            if (current < minNum) {
+                minNum = current;
             }
-            input=sc.nextLine();
+            input = sc.nextLine();
         }
         System.out.println(minNum);
     }
