f97e3c272a273a1f0e41bd1a1b260ac500227a3e
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 11:39:44 +0300

    wip

--- src/Vacation.java
+++ src/Vacation.java
@@ -5,11 +5,33 @@ public class Vacation {
         Scanner sc = new Scanner(System.in);
         double moneyNeeded = Double.parseDouble(sc.nextLine());
         double money = Double.parseDouble(sc.nextLine());
-        while () {
+        int countSpendOnARoad = 0;
+        int days=0;
+        boolean isEnoughMoney =false;
+        while (money>=moneyNeeded) {
             String action = sc.nextLine();
             double saveSpend = Double.parseDouble(sc.nextLine());
-
-
+            days++;
+            if (action.equals("spend")) {
+                countSpendOnARoad++;
+                money-=saveSpend;
+                if(countSpendOnARoad>=5){
+                    break;
+                }
+            } else {
+                money+=saveSpend;
+                if(money>=moneyNeeded){
+                    isEnoughMoney = true;
+                }
+                countSpendOnARoad=0;
+            }
+        }
+        if(isEnoughMoney){
+            System.out.printf("You saved the money for " +
+                    "%d days.",days);
+        } else {
+            System.out.println("You can't save the money.");
+            System.out.println(days);
         }
     }
 }
