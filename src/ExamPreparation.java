4ae2885daa4d02f9f2d82e3037fed299437b1bc1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-16 11:21:46 +0300

    wip

--- src/ExamPreparation.java
+++ src/ExamPreparation.java
@@ -4,32 +4,32 @@ public class ExamPreparation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int weekGrades = Integer.parseInt(sc.nextLine());
-        int countPoorGrades =0;
-        int countTasks =0;
-        double totalGrades=0;
+        int countPoorGrades = 0;
+        int countTasks = 0;
+        double totalGrades = 0;
         boolean isFailed = false;
         String input = sc.nextLine();
-        String taskName ="";
+        String taskName = "";
         while (!input.equals("Enough")) {
             taskName = input;
             int currentGrade = Integer.parseInt(sc.nextLine());
-            totalGrades+=currentGrade;
+            totalGrades += currentGrade;
             countTasks++;
             if (currentGrade <= 4) {
                 countPoorGrades++;
                 if (weekGrades == countPoorGrades) {
-                    isFailed=true;
+                    isFailed = true;
                     System.out.println("You need a break, "
-                    +weekGrades+ " poor grades.");
+                            + weekGrades + " poor grades.");
                     break;
                 }
             }
             input = sc.nextLine();
         }
         if (!isFailed) {
-            System.out.printf("Average score: %.2f\n",totalGrades/countTasks);
-            System.out.println("Number of problems: " +countTasks);
-            System.out.println("Last problem: " +taskName);
+            System.out.printf("Average score: %.2f\n", totalGrades / countTasks);
+            System.out.println("Number of problems: " + countTasks);
+            System.out.println("Last problem: " + taskName);
         }
     }
 }
